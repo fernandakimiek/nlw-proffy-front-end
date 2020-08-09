18d19ea35a1da5ee81bc3c6daeefa9c762fe2321
fernanda.kishimoto <fernanda.kishimoto@db1.com.br>
2020-08-09 11:29:00 -0300

    Formatted with Prettier

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,37 +1,35 @@
-import React from 'react'
+import React from "react";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import backIcon from '../../assets/images/icons/back.svg'
-import logoImg from '../../assets/images/logo.svg'
+import backIcon from "../../assets/images/icons/back.svg";
+import logoImg from "../../assets/images/logo.svg";
 
-import './styles.css'
+import "./styles.css";
 
 interface PageHeaderProps {
-    title: string;
-    description?: string; //prop opcional adicionar um '?'
+  title: string;
+  description?: string; //prop opcional adicionar um '?'
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
-    return (
-        <header className="page-header">
-        <div className="top-bar-container">
-            <Link to="/">
-            <img src={backIcon} alt="Voltar"/>
-            </Link>
-            <img src={logoImg} alt="Proffy"/>
-        </div>
+  return (
+    <header className="page-header">
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="Voltar" />
+        </Link>
+        <img src={logoImg} alt="Proffy" />
+      </div>
 
-        <div className="header-content">
-            <strong>{props.title}</strong>
-            { props.description && <p>{props.description}</p>}
+      <div className="header-content">
+        <strong>{props.title}</strong>
+        {props.description && <p>{props.description}</p>}
 
-            {props.children}
-        </div>
-
-        
+        {props.children}
+      </div>
     </header>
-    )
-}
+  );
+};
 
 export default PageHeader;
--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,38 +1,43 @@
-import React from 'react'
+import React from "react";
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
-import './styles.css'
+import "./styles.css";
 
 function TeacherItem() {
-    return (
-        <article className="teacher-item">
-                    <header>
-                        <img src="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/142819271/original/09dafa4104fa6aeca4e62f33326be4933ae7ccac/create-cartoon-profile-picture-abd7.jpg" alt="Proffy ANA"/>
-                   <div>
-                        <strong>Professora Ana</strong>
-                        <span>Química</span>
-                   </div>
-                    </header>
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/142819271/original/09dafa4104fa6aeca4e62f33326be4933ae7ccac/create-cartoon-profile-picture-abd7.jpg"
+          alt="Proffy ANA"
+        />
+        <div>
+          <strong>Professora Ana</strong>
+          <span>Química</span>
+        </div>
+      </header>
 
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    <br/><br/>
-                    Nullam consectetur, ante a consequat viverra, urna eros mollis ex, ut eleifend enim neque eu ligula.
-                    </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        <br />
+        <br />
+        Nullam consectetur, ante a consequat viverra, urna eros mollis ex, ut
+        eleifend enim neque eu ligula.
+      </p>
 
-                    <footer>
-                        <p>
-                        Preço/Hora
-                        <strong>R$ 70,00</strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="Whatsapp"/>
-                            Entrar em contato
-                        </button>
-                    </footer>
-                </article>
-    )
+      <footer>
+        <p>
+          Preço/Hora
+          <strong>R$ 70,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
 }
 
 export default TeacherItem;
